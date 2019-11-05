d155eca94268eda04a0adb23f4c897af90eb74c2
Isaac M <alan.burt@mackillop.act.edu.au>
2019-11-05 15:10:32 +1100

    Student Alien
    Adding student alien spritesheet to the assets folder. Loaded in the student alien asset in the load file.

--- js/load.js
+++ js/load.js
@@ -2,17 +2,17 @@
  * Use this state to load all of your assets
  */
 var loadState = {
-   preload: function() {
-      loadingLabel = game.add.text(80, 150, 'loading...', {
-         font: '30px Courier',
-         fill: '#ffffff'
-      });
+  preload: function() {
+    loadingLabel = game.add.text(80, 150, 'loading...', {
+      font: '30px Courier',
+      fill: '#ffffff'
+    });
 
-      // load all assets
-   },
+    game.load.spritesheet('studentAlien', 'assets/studentAlien.png');
+  },
 
-   create: function() {
-      game.state.start('menu');
-   }
+  create: function() {
+    game.state.start('menu');
+  }
 
 };
